7625a6bbb853d36e215a588df8525f456cead846
AllenHiggins <allenghiggins@gmail.com>
2018-05-31 12:59:37 +0100

    setting up fake api json object for image service

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -6,5 +6,17 @@ import { Injectable } from '@angular/core';
 export class ImageService {
 
   constructor() { }
-  
+
 }
+ 
+// Fake data from an api json object example
+const IMAGES = [
+
+  {"id": 1, "category": "Comic Heros", "caption": "Batman", "url": "assests/images/....."},
+  // 6
+  {"id": 2, "category": "Starwars", "caption": "Jdei", "url": "assests/images/....."},
+  // 6
+  {"id": 3, "category": "80s TV", "caption": "A-Team", "url": "assests/images/....."}
+  // 6
+
+]
