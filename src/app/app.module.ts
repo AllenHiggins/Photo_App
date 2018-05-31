9e97593e8150691297fa0ed54a4b202bc51fbd6b
AllenHiggins <allenghiggins@gmail.com>
2018-05-31 12:20:23 +0100

    added image component and image service setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageDetailComponent } from './image-detail/image-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    GalleryComponent,
+    ImageDetailComponent
   ],
   imports: [
     BrowserModule,
